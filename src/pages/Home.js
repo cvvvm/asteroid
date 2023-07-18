52216d669528010a1cb981dff86b3f8b52c57a81
cvvvm <evil_housewife@pm.me>
2023-07-17 23:51:52 -0700

    WIP home text
    added disclaimer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -69,8 +69,9 @@ function Home() {
     <>
       <div className="container vh-80 pt-5 row-gap-2">
         {/* picture */}
-        <div className="col-6 pc-center gap-1">
-          {/*           <div className="col-6 pi-center">
+        <div className="row-6">
+          <div className="col-6 pc-center gap-1">
+            {/*           <div className="col-6 pi-center">
             <p className="badge-outline ">a random photo i took</p>
             <div className="intro-image-box">
               <img
@@ -82,26 +83,30 @@ function Home() {
               more!
             </button>
           </div> */}
-          {/* text */}
+            {/* text */}
 
-          <h1 className="">{homeHeading[activeHead]}</h1>
-          <p className="">{homeDesc[activeDesc]}</p>
-          <p>
-            i built this website so you can see my work with the background color of your
-            choice.
-          </p>
+            <h1 className="">{homeHeading[activeHead]}</h1>
+            <p className="">{homeDesc[activeDesc]}</p>
+            <p>
+              i built this website<span className="note">*</span> so you can see my work
+              with the background color of your choice.
+            </p>
 
-          {/* <button className="button home-button next" onClick={nextIntro}>
+            {/* <button className="button home-button next" onClick={nextIntro}>
               next &#62;
             </button> */}
-        </div>
-        <div className="row-f10">
-          <Link to={'/about'}>
-            <button className="button-outline as-center mt-2">about</button>
-          </Link>
-          <Link to={'/projects'}>
-            <button className="button as-center mt-2">projects</button>
-          </Link>
+          </div>
+          <div className="row-f10 py-1">
+            <Link to={'/about'}>
+              <button className="button-outline as-center">about</button>
+            </Link>
+            <Link to={'/projects'}>
+              <button className="button as-center ">projects</button>
+            </Link>
+          </div>
+          <div className="col-6">
+            <p className="note">*still in development, you may find a bug or two.</p>
+          </div>
         </div>
       </div>
       <Footer />
