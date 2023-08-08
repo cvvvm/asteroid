65bec2b5aaf57e0d95535e74fb5997d892488f36
cvvvm <evil_housewife@pm.me>
2023-08-07 19:17:01 -0700

    sidebar toggling doodling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -16,27 +16,41 @@ function NavLink({ to, children, ...props }) {
   );
 }
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//
 // sidebar toggle
-//------------------------------------------------------------------------------------
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-function SidebarToggle() {
+//
+//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function SidebarToggle({ sidebarState }) {
   const sidebarToggleRef = useRef();
   const sidebarToggleContainer = useRef();
-  const [sidebarState, setSidebarState] = useState('open');
   const [mouseY, setMouseY] = useState();
 
-  // toggle track mouse Y position
+  // track mouse Y position - moves toggle
   //-----------------------------------------------------------
   useEffect(() => {
     const handleMouseMove = (event) => {
       setMouseY(event.clientY);
 
       gsap.to(sidebarToggleRef.current, {
-        top: mouseY - 70,
+        top: mouseY - 25,
         ease: 'power4.out',
         duration: 0.75,
       });
     };
+
+    // toggle boundaries
+    if (mouseY <= 50) {
+      setMouseY(50);
+    }
+    if (mouseY >= window.innerHeight - 60) {
+      setMouseY(window.innerHeight - 60);
+    }
+
     window.addEventListener('mousemove', handleMouseMove);
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
@@ -56,10 +70,48 @@ function SidebarToggle() {
   );
 }
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//
 // sidebar
-//------------------------------------------------------------------------------------
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 export default function Sidebar({ links = [], title, children }) {
+  const sidebarRef = useRef();
+  const [sidebarState, setSidebarState] = useState('closed');
+
+  // animation
+  //------------------------------------------------------------------------------------
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  // toggle sidebar state
+  //-----------------------------------------------------------
+  function toggleSidebar() {
+    if (sidebarState === 'closed') {
+      setSidebarState('open');
+    } else if (sidebarState === 'open') {
+      setSidebarState('closed');
+    }
+  }
+
+  // watch sidebar state
+  //-----------------------------------------------------------
+  useEffect(() => {
+    if (sidebarState === 'closed') {
+      gsap.to(sidebarRef.current, { translateX: 0 });
+    } else if (sidebarState === 'open') {
+      gsap.to(sidebarRef.current, { translateX: -100 });
+    }
+  }, [sidebarState]);
+
+  // content
+  //------------------------------------------------------------------------------------
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
   // map links
   //-----------------------------------------------------------
   var linksMap = links.map((link, index) => {
@@ -79,13 +131,14 @@ export default function Sidebar({ links = [], title, children }) {
   }
 
   // return sidebar
-  //-----------------------------------------------------------
+  //------------------------------------------------------------------------------------
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   return (
-    <div className="sidebar">
+    <div className="sidebar" ref={sidebarRef}>
       {title}
       {linksMap}
       {children}
-      <SidebarToggle />
+      <SidebarToggle sidebarState={sidebarState} onclick={() => toggleSidebar()} />
     </div>
   );
 }
