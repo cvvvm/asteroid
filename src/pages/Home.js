b9923a6442e6c74a20acac45961e8944c428417b
cvvvm <evil_housewife@pm.me>
2023-09-26 17:51:11 -0700

    nav font size fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ import { ColorSet } from '../functions/ThemeSet'
 export default function Home({ appColor, changeAppColor }) {
   return (
     <>
-      <div className="container vh-80 pt-7 pb-0 row-gap-4 row-gap-xs-7">
+      <div className="container vh-80 pt-7 pt-xs-9 pt-md-6 pb-0 row-gap-3 row-gap-xs-6 row-gap-md-8">
         <div className="row-8 row-gap-2">
           <div className="col-4">
             <h1 className="">hi, i'm claire.</h1>
@@ -31,7 +31,7 @@ export default function Home({ appColor, changeAppColor }) {
           <ColorSet
             currentAppColor={appColor}
             appColorTarget={changeAppColor}
-            classNames={'row-10 row-gap-1 col-gap-xxs-2 col-gap-xs-3 col-gap-sm-1'}
+            classNames={'row-10 row-gap-1 col-gap-1 col-gap-xs-3 col-gap-sm-2'}
           />
         </div>
       </div>
