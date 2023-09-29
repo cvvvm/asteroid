17bbb1d3c47a2a0775c45641710901a595c0427f
cvvvm <evil_housewife@pm.me>
2023-09-29 11:45:29 -0700

    adjusting homepage order/layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,13 +9,25 @@ import { ColorSet } from '../functions/ThemeSet'
 export default function Home({ appColor, changeAppColor }) {
   return (
     <>
-      <div className="container vh-80 pt-7 pt-xs-9 pt-md-6 pb-0 row-gap-3 row-gap-xs-6 row-gap-md-8">
-        <div className="row-8 row-gap-2">
+      <div className="container vh-80 pt-7 pt-md-6 pb-0 row-gap-3 row-gap-xs-5">
+        <div className="row-4">
           <div className="col-4">
             <h1 className="">hi, i'm claire.</h1>
             <p className="">self-taught designer, engineer, occasional illustrator.</p>
           </div>
+        </div>
 
+        {/* colors */}
+        <div className="row-f10 pi-center pc-center row-gap-0">
+          <p className="note row-f10">current color: {appColor}</p>
+          <ColorSet
+            currentAppColor={appColor}
+            appColorTarget={changeAppColor}
+            classNames={'row-10 row-gap-1 col-gap-1 col-gap-xs-3 col-gap-sm-2'}
+          />
+        </div>
+        {/* buttons */}
+        <div className="row-f10">
           <div className="row-4 pc-evenly">
             <Link to={'/about'}>
               <button className="button-outline as-center">about</button>
@@ -24,15 +36,6 @@ export default function Home({ appColor, changeAppColor }) {
               <button className="button as-center ">projects</button>
             </Link>
           </div>
-        </div>
-
-        <div className="row-f10 pi-center pc-center row-gap-1">
-          <p className="note row-f10">current color: {appColor}</p>
-          <ColorSet
-            currentAppColor={appColor}
-            appColorTarget={changeAppColor}
-            classNames={'row-10 row-gap-1 col-gap-1 col-gap-xs-3 col-gap-sm-2'}
-          />
         </div>
       </div>
       <Footer />
